2b1899d1703f6facbdc359c2a053e9400715b1c2
Paul Wang <paul.wang.is.007@gmail.com>
2021-08-21 23:12:33 -0700

    Add @EntryPoint for an Android component not supported by Hilt.

---
+++ app/src/main/java/com/paul/wang/hiltdemo/MainContentProvider.kt
@@ -0,0 +1,56 @@
+package com.paul.wang.hiltdemo
+
+import android.content.ContentProvider
+import android.content.ContentValues
+import android.database.Cursor
+import android.net.Uri
+import dagger.hilt.EntryPoint
+import dagger.hilt.InstallIn
+import dagger.hilt.android.EntryPointAccessors
+import dagger.hilt.components.SingletonComponent
+
+/**
+ * Hilt doesn't support ContentProvider ATM, add an Hilt EntryPoint.
+ */
+class MainContentProvider: ContentProvider() {
+
+    @EntryPoint
+    @InstallIn(SingletonComponent::class)
+    interface MainContentProviderEntryPoint {
+        fun getSomeString(): String
+    }
+
+    override fun onCreate(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun query(
+        p0: Uri,
+        p1: Array<out String>?,
+        p2: String?,
+        p3: Array<out String>?,
+        p4: String?
+    ): Cursor? {
+        // To use the Hilt EntryPoint
+        val mainContentProviderEntryPoint = EntryPointAccessors.fromApplication(context?.applicationContext, MainContentProviderEntryPoint::class.java)
+        val someString = mainContentProviderEntryPoint.getSomeString()
+
+        return null
+    }
+
+    override fun getType(p0: Uri): String? {
+        TODO("Not yet implemented")
+    }
+
+    override fun insert(p0: Uri, p1: ContentValues?): Uri? {
+        TODO("Not yet implemented")
+    }
+
+    override fun delete(p0: Uri, p1: String?, p2: Array<out String>?): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun update(p0: Uri, p1: ContentValues?, p2: String?, p3: Array<out String>?): Int {
+        TODO("Not yet implemented")
+    }
+}
